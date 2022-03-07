bc234ff713009f00ef7e711bcdc7c5af0079d2d6
amuthan-sakthivel <amuthan.sakthivel@clipboardhealth.com>
2022-03-07 14:01:00 +0530

    added desired capability

--- src/test/java/com/web/GoogleSearchTest.java
+++ src/test/java/com/web/GoogleSearchTest.java
@@ -2,6 +2,7 @@ package com.web;
 
 import lombok.SneakyThrows;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -14,6 +15,9 @@ public class GoogleSearchTest {
     @Test
     public void testGoogleSearch() {
         DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "chrome");
+        capabilities.setCapability("enableVNC", false);
+        capabilities.setCapability("enableVideo", false);
         WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
     }
 }
