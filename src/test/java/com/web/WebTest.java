b09cd612d01aaee9053af24536dab536cebd4368
amuthan-sakthivel <amuthan.sakthivel@clipboardhealth.com>
2022-09-03 09:02:32 +0530

    edited web test

--- src/test/java/com/web/WebTest.java
+++ src/test/java/com/web/WebTest.java
@@ -1,7 +1,10 @@
 package com.web;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.SneakyThrows;
+import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
@@ -14,11 +17,15 @@ public class WebTest {
     @SneakyThrows
     @Test
     public void testGoogleSearchUsingSelenoid() {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+        System.out.println("username :" + System.getProperty("username"));
+        System.out.println("password :" + System.getProperty("password"));
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+       /* DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("browserName", "chrome");
-        capabilities.setCapability("enableVNC", false);
+        capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", false);
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),capabilities);*/
         driver.get("https://google.co.in");
         Assert.assertEquals(driver.getTitle(), "Google");
         Thread.sleep(10000);
